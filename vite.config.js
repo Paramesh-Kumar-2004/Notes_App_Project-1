1317bc54e4eeb0630d6c66eab90f7c2ea2d565d0
Paramesh-Kumar-2004 <svpparameshkumar2004@gmail.com>
2025-11-12 12:30:14 +0530

    Ready To  Deploy

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     react(),
     tailwindcss()
   ],
-  base: "/Notes_App_Project-1/"
-})
+  base: command === 'build' ? '/Notes_App_Project-1/' : '/',
+}))
 
